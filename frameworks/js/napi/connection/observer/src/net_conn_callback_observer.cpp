729f5ebeb177e739906985b7d40a7456a697ef6f
maosiping <maosiping@huawei.com>
2022-02-22 17:58:02 +0800

    fix problem
    Signed-off-by: maosiping <maosiping@huawei.com>

--- frameworks/js/napi/connection/observer/src/net_conn_callback_observer.cpp
+++ frameworks/js/napi/connection/observer/src/net_conn_callback_observer.cpp
@@ -111,7 +111,7 @@ napi_value NetConnCallbackObserver::CreateNetCapabilities(napi_env env, NetAllCa
     NapiUtils::SetUint32Property(env, netCapabilities, KEY_LINK_UP_BAND_WIDTH_KPS, capabilities->linkUpBandwidthKbps_);
     NapiUtils::SetUint32Property(env, netCapabilities, KEY_LINK_DOWN_BAND_WIDTH_KPS,
                                  capabilities->linkDownBandwidthKbps_);
-    NETMANAGER_BASE_LOGI("capabilities->netCaps_.size() = %{public}d", capabilities->netCaps_.size());
+    NETMANAGER_BASE_LOGI("capabilities->netCaps_.size() = %{public}zu", capabilities->netCaps_.size());
     if (!capabilities->netCaps_.empty()) {
         napi_value networkCap = NapiUtils::CreateArray(env, std::min(capabilities->netCaps_.size(), MAX_ARRAY_LENGTH));
         auto it = capabilities->netCaps_.begin();
@@ -120,7 +120,7 @@ napi_value NetConnCallbackObserver::CreateNetCapabilities(napi_env env, NetAllCa
         }
         NapiUtils::SetNamedProperty(env, netCapabilities, KEY_NETWORK_CAP, networkCap);
     }
-    NETMANAGER_BASE_LOGI("capabilities->bearerTypes_.size() = %{public}d", capabilities->bearerTypes_.size());
+    NETMANAGER_BASE_LOGI("capabilities->bearerTypes_.size() = %{public}zu", capabilities->bearerTypes_.size());
     if (!capabilities->bearerTypes_.empty()) {
         napi_value bearerTypes =
             NapiUtils::CreateArray(env, std::min(capabilities->bearerTypes_.size(), MAX_ARRAY_LENGTH));
@@ -142,7 +142,7 @@ napi_value NetConnCallbackObserver::CreateConnectionProperties(napi_env env, Net
     NapiUtils::SetStringPropertyUtf8(env, connectionProperties, KEY_INTERFACE_NAME, linkInfo->ifaceName_);
     NapiUtils::SetStringPropertyUtf8(env, connectionProperties, KEY_DOMAINS, linkInfo->domain_);
     NapiUtils::SetUint32Property(env, connectionProperties, KEY_MTU, linkInfo->mtu_);
-    NETMANAGER_BASE_LOGI("linkInfo->netAddrList_.size() = %{public}d", linkInfo->netAddrList_.size());
+    NETMANAGER_BASE_LOGI("linkInfo->netAddrList_.size() = %{public}zu", linkInfo->netAddrList_.size());
     if (!linkInfo->netAddrList_.empty()) {
         napi_value linkAddresses =
             NapiUtils::CreateArray(env, std::min(linkInfo->netAddrList_.size(), MAX_ARRAY_LENGTH));
@@ -155,7 +155,7 @@ napi_value NetConnCallbackObserver::CreateConnectionProperties(napi_env env, Net
         }
         NapiUtils::SetNamedProperty(env, connectionProperties, KEY_LINK_ADDRESSES, linkAddresses);
     }
-    NETMANAGER_BASE_LOGI("linkInfo->routeList_.size() = %{public}d", linkInfo->routeList_.size());
+    NETMANAGER_BASE_LOGI("linkInfo->routeList_.size() = %{public}zu", linkInfo->routeList_.size());
     if (!linkInfo->routeList_.empty()) {
         napi_value routes = NapiUtils::CreateArray(env, std::min(linkInfo->routeList_.size(), MAX_ARRAY_LENGTH));
         auto it = linkInfo->routeList_.begin();
@@ -180,7 +180,7 @@ napi_value NetConnCallbackObserver::CreateConnectionProperties(napi_env env, Net
         }
         NapiUtils::SetNamedProperty(env, connectionProperties, KEY_ROUTES, routes);
     }
-    NETMANAGER_BASE_LOGI("linkInfo->dnsList_.size() = %{public}d", linkInfo->dnsList_.size());
+    NETMANAGER_BASE_LOGI("linkInfo->dnsList_.size() = %{public}zu", linkInfo->dnsList_.size());
     if (!linkInfo->dnsList_.empty()) {
         napi_value dnsList = NapiUtils::CreateArray(env, std::min(linkInfo->dnsList_.size(), MAX_ARRAY_LENGTH));
         auto it = linkInfo->dnsList_.begin();
