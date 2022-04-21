302a73cc6e31e859f4ba03ae690780bb9e066feb
zhangfeng <hw.zhangfeng@huawei.com>
2022-04-21 09:10:50 +0000

    #I53X4C Splitting the dhcp service into a stand-alone component
    Signed-off-by: zhangfeng <hw.zhangfeng@huawei.com>
    Change-Id: I666158b0cb1b2d640af3878ed12b6969e230b4de

--- services/netmanagernative/src/dhcp_controller.cpp
+++ services/netmanagernative/src/dhcp_controller.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Copyright (C) 2021-2022 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -58,7 +58,7 @@ void DhcpController::DhcpControllerResultNotify::OnSerExitNotify(const std::stri
 
 DhcpController::DhcpController()
 {
-    dhcpService_.reset(std::make_unique<OHOS::Wifi::DhcpService>().release());
+    dhcpService_.reset(OHOS::Wifi::DhcpServiceApi::GetInstance().release());
     dhcpResultNotify_.reset(std::make_unique<DhcpControllerResultNotify>(*this).release());
 }
 
