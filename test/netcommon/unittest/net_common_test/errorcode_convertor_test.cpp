046b27a91b44d2bd411a8f899aa18525172c10d9
liyufan123 <liyufan5@huawei.com>
2022-11-28 20:55:59 +0800

    add TDD
    Signed-off-by: liyufan123 <liyufan5@huawei.com>

---
+++ test/netcommon/unittest/net_common_test/errorcode_convertor_test.cpp
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include "errorcode_convertor.h"
+#include "net_manager_constants.h"
+
+namespace OHOS {
+namespace NetManagerStandard {
+namespace {
+using namespace testing::ext;
+} // namespace
+
+class ErrorCodeConvertorTest : public testing::Test {
+public:
+    static void SetUpTestCase();
+    static void TearDownTestCase();
+    void SetUp();
+    void TearDown();
+};
+
+void ErrorCodeConvertorTest::SetUpTestCase() {}
+
+void ErrorCodeConvertorTest::TearDownTestCase() {}
+
+void ErrorCodeConvertorTest::SetUp() {}
+
+void ErrorCodeConvertorTest::TearDown() {}
+
+HWTEST_F(ErrorCodeConvertorTest, ConvertErrorCodeTest001, TestSize.Level1)
+{
+    auto instance = std::make_unique<ErrorCodeConvertor>();
+    int32_t errorCode = 11;
+    auto ret = instance->ConvertErrorCode(errorCode);
+    ASSERT_TRUE(ret.empty());
+}
+
+HWTEST_F(ErrorCodeConvertorTest, ConvertErrorCodeTest002, TestSize.Level1)
+{
+    int32_t testErrorCode = 5445645;
+    std::unique_ptr<ErrorCodeConvertor> instance = std::make_unique<NetBaseErrorCodeConvertor>();
+    auto ret = instance->ConvertErrorCode(NETMANAGER_ERR_ADD_DEATH_RECIPIENT_FAIL);
+    ASSERT_FALSE(ret.empty());
+    ret = instance->ConvertErrorCode(testErrorCode);
+    ASSERT_TRUE(ret.empty());
+}
+} // namespace NetManagerStandard
+} // namespace OHOS
