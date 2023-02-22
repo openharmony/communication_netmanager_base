ab6ce7408aa035ceea3eccc2c8e8c9b67f647d3a
张海丰 <zhanghaifeng11@huawei.com>
2023-02-22 06:39:09 +0000

    update utils/errorcode_utils/src/errorcode_convertor.cpp.
    Signed-off-by: 张海丰 <zhanghaifeng11@huawei.com>

--- utils/errorcode_utils/src/errorcode_convertor.cpp
+++ utils/errorcode_utils/src/errorcode_convertor.cpp
@@ -123,38 +123,7 @@ std::map<int32_t, const char *> g_errStringMap = {
     {NET_CONN_ERR_CREATE_DUMP_FAILED, "create dump failed"},
 };
 } // namespace
-std::string NetBaseErrorCodeConvertor::ConvertErrorCode(int32_t &errorCode)
-{
-    errorMap_[NETMANAGER_SUCCESS] = "successful";
-    errorMap_[NETMANAGER_ERR_MEMCPY_FAIL] = "memcpy fail";
-    errorMap_[NETMANAGER_ERR_MEMSET_FAIL] = "memset fail";
-    errorMap_[NETMANAGER_ERR_STRCPY_FAIL] = "strcpy fail";
-    errorMap_[NETMANAGER_ERR_STRING_EMPTY] = "string is null";
-    errorMap_[NETMANAGER_ERR_LOCAL_PTR_NULL] = "pointer is null";
-    errorMap_[NETMANAGER_ERR_DESCRIPTOR_MISMATCH] = "ipc stub mismatch descriptor";
-    errorMap_[NETMANAGER_ERR_WRITE_DESCRIPTOR_TOKEN_FAIL] = "ipc proxy write descriptor token fail";
-    errorMap_[NETMANAGER_ERR_WRITE_DATA_FAIL] = "ipc proxy write data fail";
-    errorMap_[NETMANAGER_ERR_WRITE_REPLY_FAIL] = "ipc proxy write reply fail";
-    errorMap_[NETMANAGER_ERR_READ_DATA_FAIL] = "ipc stub read data fail";
-    errorMap_[NETMANAGER_ERR_READ_REPLY_FAIL] = "ipc stub read reply fail";
-    errorMap_[NETMANAGER_ERR_IPC_CONNECT_STUB_FAIL] = "ipc connect stub fail";
-    errorMap_[NETMANAGER_ERR_PERMISSION_DENIED] = "permission denied";
-    errorMap_[NETMANAGER_ERR_PARAMETER_ERROR] = "parameter error";
-    errorMap_[NETMANAGER_ERR_ADD_DEATH_RECIPIENT_FAIL] = "ipc add death recipient fail";
-    errorMap_[NETMANAGER_ERR_REGISTER_CALLBACK_FAIL] = "register callback fail";
-    errorMap_[NETMANAGER_ERR_UNINIT] = "uninit error";
-    errorMap_[NETMANAGER_ERR_CAPABILITY_NOT_SUPPORTED] = "capability not supported";
-}
 
-std::string ErrorCodeConvertor::ConvertErrorCode(int32_t errorCode)
-{
-    return "";
-}
-
-NetBaseErrorCodeConvertor::NetBaseErrorCodeConvertor()
-{
-    errorMap_[NET_CONN_ERR_INVALID_SUPPLIER_ID] = "invalid supplier id";
-}
 std::map<int32_t, const char *> g_errStringMap = {
 
     /* Net mDNS Manager */
@@ -170,10 +139,8 @@ std::map<int32_t, const char *> g_errStringMap = {
     {NET_MDNS_ERR_SEND, "Send packet failed"}
 };
 
-std::string NetBaseErrorCodeConvertor::ConvertErrorCode(int32_t errorCode)
+std::string NetBaseErrorCodeConvertor::ConvertErrorCode(int32_t &errorCode)
 {
-    if (errorMap_.find(errorCode) == errorMap_.end()) {
-        return "";
     if (errorCode > ERROR_DIVISOR && (errorCode % ERROR_DIVISOR) >= ERROR_RANGE) {
         if (g_errNumMap.find(errorCode) != g_errNumMap.end()) {
             errorCode = g_errNumMap.at(errorCode);
