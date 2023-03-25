3bc8b2f5dce64fb12d050a2723af54f8adbc0644
Yangys <yangyousheng@huawei.com>
2023-03-25 10:16:25 +0800

    Codecheck
    Signed-off-by: Yangys <yangyousheng@huawei.com>

--- services/netconnmanager/src/network.cpp
+++ services/netconnmanager/src/network.cpp
@@ -208,11 +208,11 @@ void Network::UpdateRoutes(const NetLinkInfo &netLinkInfo)
     NETMGR_LOG_D("UpdateRoutes, old routes: [%{public}s]", netLinkInfo_.ToStringRoute("").c_str());
     for (const auto &route : netLinkInfo_.routeList_) {
         std::string destAddress = route.destination_.address_ + "/" + std::to_string(route.destination_.prefixlen_);
-        auto ret = static_cast<uint32_t>(NetsysController::GetInstance().NetworkRemoveRoute(netId_, route.iface_, destAddress,
-                                                                         route.gateway_.address_));
+        auto ret = static_cast<uint32_t>(NetsysController::GetInstance().NetworkRemoveRoute(
+                netId_, route.iface_, destAddress, route.gateway_.address_));
         if (route.destination_.address_ != LOCAL_ROUTE_NEXT_HOP) {
-            ret |= static_cast<uint32_t>(NetsysController::GetInstance().NetworkRemoveRoute(LOCAL_NET_ID, route.iface_, destAddress,
-                                                                      LOCAL_ROUTE_NEXT_HOP));
+            ret |= static_cast<uint32_t>(NetsysController::GetInstance().NetworkRemoveRoute(
+                LOCAL_NET_ID, route.iface_, destAddress, LOCAL_ROUTE_NEXT_HOP));
         }
         if (ret != NETMANAGER_SUCCESS) {
             SendSupplierFaultHiSysEvent(FAULT_UPDATE_NETLINK_INFO_FAILED, ERROR_MSG_REMOVE_NET_ROUTES_FAILED);
