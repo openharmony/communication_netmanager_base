22a2ddfb5448c9226c8c33f4033153ecd6dec5e1
xujie <xujie223@huawei.com>
2023-07-14 11:24:11 +0800

    codecheck
    Signed-off-by: xujie <xujie223@huawei.com>

--- services/netmanagernative/src/netsys_native_service_stub.cpp
+++ services/netmanagernative/src/netsys_native_service_stub.cpp
@@ -1133,7 +1133,7 @@ int32_t NetsysNativeServiceStub::CmdGetAllStatsInfo(MessageParcel &data, Message
 int32_t NetsysNativeServiceStub::CmdSetIptablesCommandForRes(MessageParcel &data, MessageParcel &reply)
 {
     if (!NetManagerStandard::NetManagerPermission::CheckNetSysInternalPermission(
-            NetManagerStandard::Permission::NETSYS_INTERNAL)) {
+        NetManagerStandard::Permission::NETSYS_INTERNAL)) {
         NETNATIVE_LOGE("CmdSetIptablesCommandForRes CheckNetSysInternalPermission failed");
         return NETMANAGER_ERR_PERMISSION_DENIED;
     }
