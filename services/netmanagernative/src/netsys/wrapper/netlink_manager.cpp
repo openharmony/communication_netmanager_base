f04cc7c0c6b2499eaece75e6b3f88c0501207f72
xujie <xujie223@huawei.com>
2023-08-09 00:09:05 +0800

    fix
    Signed-off-by: xujie <xujie223@huawei.com>

--- services/netmanagernative/src/netsys/wrapper/netlink_manager.cpp
+++ services/netmanagernative/src/netsys/wrapper/netlink_manager.cpp
@@ -62,8 +62,8 @@ bool CreateNetlinkDistributor(int32_t netlinkType, const DistributorParam &param
     sockAddr.nl_groups = param.groups;
 
     if ((socketFd = socket(PF_NETLINK, SOCK_DGRAM | SOCK_CLOEXEC, netlinkType)) < 0) {
-        NETNATIVE_LOGE("Creat socket for family failed NetLinkType is %{public}d: %{public}s = %{public}d",
-                       netlinkType, strerror(errno), errno);
+        NETNATIVE_LOGE("Creat socket for family failed NetLinkType is %{public}d: %{public}s = %{public}d", netlinkType,
+                       strerror(errno), errno);
         return false;
     }
 
