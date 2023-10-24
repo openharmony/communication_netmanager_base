eb958e3463ed836dac9726ccad19020a43a71307
xujie <xujie223@huawei.com>
2023-10-24 10:33:13 +0800

    modify failed delay time to 1s
    Signed-off-by: xujie <xujie223@huawei.com>

--- services/netconnmanager/src/net_monitor.cpp
+++ services/netconnmanager/src/net_monitor.cpp
@@ -41,7 +41,7 @@
 namespace OHOS {
 namespace NetManagerStandard {
 namespace {
-constexpr int32_t INIT_DETECTION_DELAY_MS = 8 * 1000;
+constexpr int32_t INIT_DETECTION_DELAY_MS = 1 * 1000;
 constexpr int32_t MAX_FAILED_DETECTION_DELAY_MS = 5 * 60 * 1000;
 constexpr int32_t CAPTIVE_PORTAL_DETECTION_DELAY_MS = 60 * 1000;
 constexpr int32_t DOUBLE = 2;
