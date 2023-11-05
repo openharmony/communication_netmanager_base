de453d2af5e7c781ca6ffe2d871d21f4ba8064de
liuxiyao223 <liuxiyao223@huawei.com>
2023-11-05 11:42:33 +0800

    test xts
    Signed-off-by: liuxiyao223 <liuxiyao223@huawei.com>

--- services/netmanagernative/src/netsys/dnsresolv/dns_resolv_listen.cpp
+++ services/netmanagernative/src/netsys/dnsresolv/dns_resolv_listen.cpp
@@ -247,15 +247,16 @@ void DnsResolvListen::ProcPostDnsResultCommand(int clientSockFd, uint16_t netId)
     }
 
     if ((queryret == 0) && (ai_size > 0)) {
-        ai_size = std::min<uint32_t>(MAX_RESULTS, ai_size);
-        AddrInfo addrInfo[MAX_RESULTS] = {};
-        if (!PollRecvData(clientSockFd, reinterpret_cast<char *>(addrInfo), sizeof(AddrInfo) * ai_size)) {
-            NETNATIVE_LOGE("read errno %{public}d", errno);
-            close(clientSockFd);
-            return;
-        }
-        DnsQualityDiag::GetInstance().ReportDnsResult(netid, uid, pid, usedtime, name,
-                                                      ai_size, queryret, param, addrInfo);
+        // ai_size = std::min<uint32_t>(MAX_RESULTS, ai_size);
+        // AddrInfo addrInfo[MAX_RESULTS] = {};
+        // if (!PollRecvData(clientSockFd, reinterpret_cast<char *>(addrInfo), sizeof(AddrInfo) * ai_size)) {
+        //     NETNATIVE_LOGE("read errno %{public}d", errno);
+        //     close(clientSockFd);
+        //     return;
+        //}
+        // DnsQualityDiag::GetInstance().ReportDnsResult(netid, uid, pid, usedtime, name,
+        //                                              ai_size, queryret, param, addrInfo);
+	NETNATIVE_LOGI("ProcPostDnsResultCommand end2");
     } else {
         DnsQualityDiag::GetInstance().ReportDnsResult(netid, uid, pid, usedtime, name, 0, queryret, param, nullptr);
     }
