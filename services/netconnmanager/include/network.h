895f14a17829748ab3ed00d3ca523b1166aba91d
liuxiyao223 <liuxiyao223@huawei.com>
2023-11-27 10:25:51 +0800

    Fix crash
    Signed-off-by: liuxiyao223 <liuxiyao223@huawei.com>

--- services/netconnmanager/include/network.h
+++ services/netconnmanager/include/network.h
@@ -77,7 +77,6 @@ private:
     void SendConnectionChangedBroadcast(const NetConnState &netConnState) const;
     void SendSupplierFaultHiSysEvent(NetConnSupplerFault errorType, const std::string &errMsg);
     void ResetNetlinkInfo();
-    void NetDetectionForDnsHealthSync(bool dnsHealthSuccess);
     bool IsDetectionForDnsSuccess(NetDetectionStatus netDetectionState, bool dnsHealthSuccess);
     bool IsDetectionForDnsFail(NetDetectionStatus netDetectionState, bool dnsHealthSuccess);
 
--- services/netconnmanager/src/network.cpp
+++ services/netconnmanager/src/network.cpp
@@ -425,16 +425,6 @@ void Network::StartNetDetection(bool needReport)
 void Network::NetDetectionForDnsHealth(bool dnsHealthSuccess)
 {
     NETMGR_LOG_I("Enter Network::NetDetectionForDnsHealth");
-    if (eventHandler_) {
-        eventHandler_ -> PostSyncTask([dnsHealthSuccess, this]() {
-            this->NetDetectionForDnsHealthSync(dnsHealthSuccess);
-        });
-    }
-}
-
-void Network::NetDetectionForDnsHealthSync(bool dnsHealthSuccess)
-{
-    NETMGR_LOG_I("Enter Network::NetDetectionForDnsHealthSync");
     if (netMonitor_ == nullptr) {
         NETMGR_LOG_E("netMonitor_ is nullptr");
         return;
