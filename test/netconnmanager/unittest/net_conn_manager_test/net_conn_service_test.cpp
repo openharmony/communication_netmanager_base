db510bc436794dfc2a75122e26da1b7700c5a6c5
liuxiyao223 <liuxiyao223@huawei.com>
2023-12-04 11:38:32 +0800

    remove redundant OnAddSystemAbilityTest001
    Signed-off-by: liuxiyao223 <liuxiyao223@huawei.com>

--- test/netconnmanager/unittest/net_conn_manager_test/net_conn_service_test.cpp
+++ test/netconnmanager/unittest/net_conn_manager_test/net_conn_service_test.cpp
@@ -172,7 +172,13 @@ void NetConnServiceTest::SetUp() {}
 
 void NetConnServiceTest::TearDown() {}
 
-HWTEST_F(NetConnServiceTest, OnAddSystemAbilityTest001, TestSize.Level1)
+HWTEST_F(NetConnServiceTest, OnRemoveSystemAbility001, TestSize.Level1)
+{
+    std::string deviceId = "dev1";
+    NetConnService::GetInstance()->OnRemoveSystemAbility(COMM_NETSYS_NATIVE_SYS_ABILITY_ID, deviceId);
+}
+
+HWTEST_F(NetConnServiceTest, OnAddSystemAbility001, TestSize.Level1)
 {
     std::string deviceId = "dev1";
     NetConnService::GetInstance()->OnAddSystemAbility(COMM_NETSYS_NATIVE_SYS_ABILITY_ID, deviceId);
@@ -830,18 +836,6 @@ HWTEST_F(NetConnServiceTest, NetDetectionForDnsHealthTest001, TestSize.Level1)
     EXPECT_EQ(ret, NETMANAGER_SUCCESS);
     ret = NetConnService::GetInstance()->NetDetectionForDnsHealth(netId, dnsHealthFail);
     EXPECT_EQ(ret, NETMANAGER_SUCCESS);
-}
-
-HWTEST_F(NetConnServiceTest, OnRemoveSystemAbility001, TestSize.Level1)
-{
-    std::string deviceId = "dev1";
-    NetConnService::GetInstance()->OnRemoveSystemAbility(COMM_NETSYS_NATIVE_SYS_ABILITY_ID, deviceId);
-}
-
-HWTEST_F(NetConnServiceTest, OnAddSystemAbility001, TestSize.Level1)
-{
-    std::string deviceId = "dev1";
-    NetConnService::GetInstance()->OnAddSystemAbility(COMM_NETSYS_NATIVE_SYS_ABILITY_ID, deviceId);
 }
 
 HWTEST_F(NetConnServiceTest, NetConnServiceBranchTest003, TestSize.Level1)
