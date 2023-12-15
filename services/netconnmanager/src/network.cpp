8f7f0bce685401bbdf280ec6585c03cc5dff796e
liuxiyao223 <liuxiyao223@huawei.com>
2023-12-15 18:36:59 +0800

    update code by review suggestion
    Signed-off-by: liuxiyao223 <liuxiyao223@huawei.com>

--- services/netconnmanager/src/network.cpp
+++ services/netconnmanager/src/network.cpp
@@ -232,8 +232,7 @@ void Network::UpdateIpAddrs(const NetLinkInfo &newNetLinkInfo)
 {
     // netLinkInfo_ represents the old, netLinkInfo represents the new
     // Update: remove old Ips first, then add the new Ips
-    NETMGR_LOG_I("UpdateIpAddrs, old ip addrs size: [%{public}d]",
-        static_cast<int>(netLinkInfo_.netAddrList_.size()));
+    NETMGR_LOG_I("UpdateIpAddrs, old ip addrs size: [%{public}zu]", netLinkInfo_.netAddrList_.size());
     for (const auto &inetAddr : netLinkInfo_.netAddrList_) {
         if (newNetLinkInfo.HasNetAddr(inetAddr)) {
             NETMGR_LOG_W("Same ip address:[%{public}s], there is not need to be deleted",
@@ -261,8 +260,7 @@ void Network::UpdateIpAddrs(const NetLinkInfo &newNetLinkInfo)
         });
     }
 
-    NETMGR_LOG_I("UpdateIpAddrs, new ip addrs size: [%{public}d]",
-        static_cast<int>(newNetLinkInfo.netAddrList_.size()));
+    NETMGR_LOG_I("UpdateIpAddrs, new ip addrs size: [%{public}zu]", newNetLinkInfo.netAddrList_.size());
     for (const auto &inetAddr : newNetLinkInfo.netAddrList_) {
         if (netLinkInfo_.HasNetAddr(inetAddr)) {
             NETMGR_LOG_W("Same ip address:[%{public}s], there is no need to add it again",
