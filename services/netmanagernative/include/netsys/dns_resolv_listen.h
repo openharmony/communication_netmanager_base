10b11315bac985bedbf2b4f3f6f16ce15c80be43
SubmarinePhantom <liuxiyao223@huawei.com>
2024-02-23 14:24:53 +0800

    超大函数整改
    Signed-off-by: SubmarinePhantom <liuxiyao223@huawei.com>

--- services/netmanagernative/include/netsys/dns_resolv_listen.h
+++ services/netmanagernative/include/netsys/dns_resolv_listen.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2022-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -39,6 +39,7 @@ private:
     static void ProcBindSocketCommand(int clientSockFd, uint16_t netId);
     static void ProcCommand(int clientSockFd);
     static int32_t ProcGetKeyForCache(int clientSockFd, char *name);
+    static bool ProcPostDnsThreadResult(int clientSockFd, uint32_t &uid, uint32_t &pid);
 
     int32_t serverSockFd_;
     std::shared_ptr<AppExecFwk::EventRunner> dnsResolvRunner_ = nullptr;
