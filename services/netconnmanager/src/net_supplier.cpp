3e1243290af33a19d26f4784559bcc649a274e29
shiminghui <shiminghui6@huawei.com>
2024-04-25 21:07:23 +0800

    网卡和simId映射关系传入
    Signed-off-by: shiminghui <shiminghui6@huawei.com>

--- services/netconnmanager/src/net_supplier.cpp
+++ services/netconnmanager/src/net_supplier.cpp
@@ -79,7 +79,9 @@ int32_t NetSupplier::UpdateNetLinkInfo(NetLinkInfo &netLinkInfo)
         return NET_CONN_ERR_INVALID_NETWORK;
     }
 
-    netLinkInfo.ident_ = GetNetSupplierIdent().substr(strlen(SIMID_IDENT_PREFIX));
+    if (GetNetSupplierIdent().substr(0, strlen(SIMID_IDENT_PREFIX)) == SIMID_IDENT_PREFIX) {
+        netLinkInfo.ident_ = GetNetSupplierIdent().substr(strlen(SIMID_IDENT_PREFIX));
+    }
     NETMGR_LOG_D("Update netlink info: netLinkInfo[%{public}s]", netLinkInfo.ToString(" ").c_str());
     if (!network_->UpdateNetLinkInfo(netLinkInfo)) {
         return NET_CONN_ERR_SERVICE_UPDATE_NET_LINK_INFO_FAIL;
