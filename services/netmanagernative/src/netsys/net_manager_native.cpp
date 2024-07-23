1ef572f8e31627d29f6553a154c21079e0ed9c9a
wulimeimei <hanjing5@huawei.com>
2024-07-23 10:34:05 +0800

    Fix unittest cases.
    Signed-off-by: wulimeimei <hanjing5@huawei.com>

--- services/netmanagernative/src/netsys/net_manager_native.cpp
+++ services/netmanagernative/src/netsys/net_manager_native.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -27,7 +27,6 @@
 #include "vnic_manager.h"
 
 using namespace OHOS::NetManagerStandard::CommonUtils;
-std::vector<uint32_t> OHOS::nmd::NetManagerNative::interfaceIdex_;
 
 namespace OHOS {
 namespace nmd {
