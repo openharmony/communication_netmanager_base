f34b749efa80037c85fd62232e2927fe00f46740
ACatNamedTango <acatnamedtango@163.com>
2024-08-29 16:47:58 +0800

    fix testcases
    Signed-off-by: ACatNamedTango <acatnamedtango@163.com>

--- test/netmanagernative/unittest/bandwidth_manager_test/bandwidth_manager_test.cpp
+++ test/netmanagernative/unittest/bandwidth_manager_test/bandwidth_manager_test.cpp
@@ -14,6 +14,8 @@
  */
 
 #include <gtest/gtest.h>
+#include <chrono>
+#include <thread>
 
 #include "netsys_controller.h"
 
@@ -39,7 +41,10 @@ void BandwidthManagerTest::SetUpTestCase()
     NetsysController::GetInstance().BandwidthEnableDataSaver(false);
 }
 
-void BandwidthManagerTest::TearDownTestCase() {}
+void BandwidthManagerTest::TearDownTestCase()
+{
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+}
 
 void BandwidthManagerTest::SetUp() {}
 
