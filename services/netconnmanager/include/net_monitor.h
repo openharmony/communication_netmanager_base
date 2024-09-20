37b5c197f415a72fa9633b97ee70218f883bcdf7
Aurora <liuxiyao223@huawei.com>
2024-09-20 17:10:17 +0800

    Fix netmanager freeze
    Signed-off-by: Aurora <liuxiyao223@huawei.com>

--- services/netconnmanager/include/net_monitor.h
+++ services/netconnmanager/include/net_monitor.h
@@ -23,8 +23,11 @@
 
 #include "refbase.h"
 
+#include "system_ability.h"
+#include "system_ability_definition.h"
 #include "i_net_monitor_callback.h"
 #include "net_conn_types.h"
+#include "net_datashare_utils.h"
 #include "net_http_probe.h"
 #include "net_link_info.h"
 #include "probe_thread.h"
@@ -91,6 +94,7 @@ private:
     NetHttpProbeResult GetThreadDetectResult(std::shared_ptr<ProbeThread>& probeThread, ProbeType probeType);
     void GetHttpProbeUrlFromConfig();
     void GetDetectUrlConfig();
+    bool CheckIfSettingsDataReady();
 
 private:
     uint32_t netId_ = 0;
@@ -112,6 +116,7 @@ private:
     std::string fallbackHttpsUrl_;
     std::mutex proxyMtx_;
     bool isNeedSuffix_ = false;
+    bool isDataShareReady_ = false;
 };
 } // namespace NetManagerStandard
 } // namespace OHOS
