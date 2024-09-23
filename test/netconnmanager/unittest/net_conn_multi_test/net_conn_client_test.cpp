c5357a74bb761ed4e5de105c34e6215097d54f17
yinyongbin <yinyongbin@huawei.com>
2024-09-23 14:57:58 +0000

    update test/netconnmanager/unittest/net_conn_multi_test/net_conn_client_test.cpp.
    Signed-off-by: yinyongbin <yinyongbin@huawei.com>

--- test/netconnmanager/unittest/net_conn_multi_test/net_conn_client_test.cpp
+++ test/netconnmanager/unittest/net_conn_multi_test/net_conn_client_test.cpp
@@ -1416,14 +1416,16 @@ HWTEST_F(NetConnClientTest, UpdateSupplierScore001, TestSize.Level1)
 {
     NetManagerBaseAccessToken token;
     uint32_t supplierId = 100;
-    int32_t ret = NetConnClient::GetInstance().UpdateSupplierScore(NetBearType::BEARER_WIFI, QUALITY_POOR_STATE, supplierId);
+    int32_t ret = NetConnClient::GetInstance().UpdateSupplierScore(NetBearType::BEARER_WIFI,
+        QUALITY_POOR_STATE, supplierId);
     EXPECT_NE(ret, NETMANAGER_ERR_PERMISSION_DENIED);
 }
 
 HWTEST_F(NetConnClientTest, UpdateSupplierScore002, TestSize.Level1)
 {
     uint32_t supplierId = 100;
-    int32_t ret = NetConnClient::GetInstance().UpdateSupplierScore(NetBearType::BEARER_WIFI, QUALITY_POOR_STATE, supplierId);
+    int32_t ret = NetConnClient::GetInstance().UpdateSupplierScore(NetBearType::BEARER_WIFI,
+        QUALITY_POOR_STATE, supplierId);
     EXPECT_EQ(ret, NETMANAGER_ERR_PERMISSION_DENIED);
 }
 
