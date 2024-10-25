9d7cc7154a9f62146f23574639e736542084a928
zhouxj <zhouxj1111@thundersoft.com>
2024-10-25 11:40:14 +0800

    netmanager crash
    Signed-off-by: zhouxj <zhouxj1111@thundersoft.com>
    Change-Id: I6ffe18be85d5de174cee714514bc3b11dc8f19fe

--- services/netconnmanager/src/net_conn_service.cpp
+++ services/netconnmanager/src/net_conn_service.cpp
@@ -2584,31 +2584,37 @@ bool NetConnService::IsSupplierMatchRequestAndNetwork(sptr<NetSupplier> ns)
 
 void NetConnService::OnNetSysRestart()
 {
-    NETMGR_LOG_I("OnNetSysRestart");
 
-    std::unique_lock<std::recursive_mutex> locker(netManagerMutex_);
-    NET_SUPPLIER_MAP::iterator iter;
-    for (iter = netSuppliers_.begin(); iter != netSuppliers_.end(); ++iter) {
-        if (iter->second == nullptr) {
-            continue;
-        }
+    if (netConnEventHandler_) {
+        netConnEventHandler_->PostSyncTask([this]() {
+            NETMGR_LOG_I("OnNetSysRestart");
 
-        NETMGR_LOG_D("supplier info, supplier[%{public}d, %{public}s], realScore[%{public}d], isConnected[%{public}d]",
-            iter->second->GetSupplierId(), iter->second->GetNetSupplierIdent().c_str(),
-            iter->second->GetRealScore(), iter->second->IsConnected());
-
-        if ((!iter->second->IsConnected()) || (!IsSupplierMatchRequestAndNetwork(iter->second))) {
-            NETMGR_LOG_D("Supplier[%{public}d] is not connected or not match request.", iter->second->GetSupplierId());
-            continue;
-        }
-
-        iter->second->ResumeNetworkInfo();
+            NET_SUPPLIER_MAP::iterator iter;
+            for (iter = this->netSuppliers_.begin(); iter != this->netSuppliers_.end(); ++iter) {
+                if (iter->second == nullptr) {
+                    continue;
+                }
+        
+                NETMGR_LOG_D("supplier info, supplier[%{public}d, %{public}s], realScore[%{public}d], isConnected[%{public}d]",
+                    iter->second->GetSupplierId(), iter->second->GetNetSupplierIdent().c_str(),
+                    iter->second->GetRealScore(), iter->second->IsConnected());
+        
+                if ((!iter->second->IsConnected()) || (!this->IsSupplierMatchRequestAndNetwork(iter->second))) {
+                    NETMGR_LOG_D("Supplier[%{public}d] is not connected or not match request.", iter->second->GetSupplierId());
+                    continue;
+                }
+        
+                iter->second->ResumeNetworkInfo();
+            }
+            if (this->defaultNetSupplier_ != nullptr) {
+                this->defaultNetSupplier_->ClearDefault();
+                this->defaultNetSupplier_ = nullptr;
+            }
+            this->FindBestNetworkForAllRequest();
+        });
     }
-    if (defaultNetSupplier_ != nullptr) {
-        defaultNetSupplier_->ClearDefault();
-        defaultNetSupplier_ = nullptr;
-    }
-    FindBestNetworkForAllRequest();
+
+    
 }
 
 int32_t NetConnService::IsPreferCellularUrl(const std::string& url, bool& preferCellular)
