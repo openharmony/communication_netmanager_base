435af582d42bc5f509cd7ad062be6cc0a66babd2
wendan4 <wendan4@huawei.com>
2025-02-12 03:11:57 +0000

    update test/netmanagernative/unittest/netsys_distributed_test/netsys_native_service_stub_test.cpp.
    Signed-off-by: wendan4 <wendan4@huawei.com>

--- test/netmanagernative/unittest/netsys_distributed_test/netsys_native_service_stub_test.cpp
+++ test/netmanagernative/unittest/netsys_distributed_test/netsys_native_service_stub_test.cpp
@@ -384,6 +384,11 @@ public:
         return 0;
     }
 
+    int32_t GetNetworkCellularSharingTraffic(NetworkSharingTraffic &traffic, std::string &ifaceName) override
+    {
+        return 0;
+    }
+
     int32_t GetTotalStats(uint64_t &stats, uint32_t type) override
     {
         return 0;
