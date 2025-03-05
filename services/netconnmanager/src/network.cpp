9b16632f943be329ab5d927f02628a0cfcbcae4f
harrey <harrey.huang@huawei.com>
2025-03-05 06:39:03 +0000

    update services/netconnmanager/src/network.cpp.
    Signed-off-by: harrey <harrey.huang@huawei.com>

--- services/netconnmanager/src/network.cpp
+++ services/netconnmanager/src/network.cpp
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #include "common_event_support.h"
 
 #include "broadcast_manager.h"
@@ -375,7 +376,7 @@ void Network::HandleUpdateIpAddrs(const NetLinkInfo &newNetLinkInfo)
         std::shared_lock<std::shared_mutex> lock(netLinkInfoMutex_);
         if (netLinkInfo_.HasNetAddr(inetAddr)) {
             NETMGR_LOG_W("Same ip address:[%{public}s], there is no need to add it again",
-                        CommonUtils::ToAnonymousIp(inetAddr.address_).c_str());
+                       CommonUtils::ToAnonymousIp(inetAddr.address_).c_str());
             continue;
         }
         lock.unlock();
