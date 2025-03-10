985c7ef41126b177618358e791de383e883bd4da
xhttt123 <2317512661@qq.com>
2025-03-10 11:48:26 +0000

    update services/netpolicymanager/src/firewall_rule/firewall_rule.cpp.
    Signed-off-by: xhttt123 <2317512661@qq.com>

--- services/netpolicymanager/src/firewall_rule/firewall_rule.cpp
+++ services/netpolicymanager/src/firewall_rule/firewall_rule.cpp
@@ -12,7 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+// t
 #include "firewall_rule.h"
 
 #include "device_idle_firewall_rule.h"
