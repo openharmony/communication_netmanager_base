9633c965e1d17790711c6c6aa3c2ebcf6e66ce3d
李泽凡 <lizefan3@huawei.com>
2025-04-29 07:53:40 +0000

    update frameworks/native/netstatsclient/src/net_push_stats_info.cpp.
    Signed-off-by: 李泽凡 <lizefan3@huawei.com>

--- frameworks/native/netstatsclient/src/net_push_stats_info.cpp
+++ frameworks/native/netstatsclient/src/net_push_stats_info.cpp
@@ -78,7 +78,7 @@ bool PushStatsInfo::Marshalling(Parcel &parcel, const PushStatsInfo &info)
 
 PushStatsInfo* PushStatsInfo::Unmarshalling(Parcel &parcel)
 {
-    std::unique_ptr<PushStatsInfo> stats(new (std::nothrow) PushStatsInfo());
+    std::unique_ptr<PushStatsInfo> stats = std::make_unique<PushStatsInfo>();
     if (stats == nullptr) {
         return nullptr;
     }
