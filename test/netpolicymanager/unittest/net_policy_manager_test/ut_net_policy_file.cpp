26884fa399a048c032d2152bee63ae27678e2f70
zhengsiwen960323 <zhengsiwen2@huawei.com>
2025-05-21 07:27:24 +0000

    update test/netpolicymanager/unittest/net_policy_manager_test/ut_net_policy_file.cpp.
    Signed-off-by: zhengsiwen960323 <zhengsiwen2@huawei.com>

--- test/netpolicymanager/unittest/net_policy_manager_test/ut_net_policy_file.cpp
+++ test/netpolicymanager/unittest/net_policy_manager_test/ut_net_policy_file.cpp
@@ -114,82 +114,28 @@ HWTEST_F(UtNetPolicyFile, NetPolicyFile002, TestSize.Level1)
 }
 
 /**
- * @tc.name: SetDeviceIdleTrustlist001
- * @tc.desc: Test SetDeviceIdleTrustlist Func.
+ * @tc.name: Json2Obj001
+ * @tc.desc: Test Json2Obj WriteFirewallRules.
  * @tc.type: FUNC
  */
-HWTEST_F(UtNetPolicyFirewall, SetDeviceIdleTrustlist001, TestSize.Level1)
+HWTEST_F(UtNetPolicyFile, Json2Obj001, TestSize.Level1)
 {
-    const uint32_t uid = 101;
-    std::vector<uint32_t> initialUids(1001);
-    for (size_t i = 0; i < initialUids.size(); ++i) {
-        initialUids[i] = i;
-    }
-    for (uint32_t testuid : initialUids) {
-        netPolicyFirewall_->powerSaveAllowedList_.insert(testuid);
-    }
-    auto result = netPolicyFirewall_->SetDeviceIdleTrustlist({uid}, true);
-    EXPECT_EQ(result, NETMANAGER_ERR_PARAMETER_ERROR);
+    std::string content = "";
+    NetPolicy netpolicy;
+    auto result = netPolicyFile_->Json2Obj(content, netPolicy);
+    EXPECT_EQ(result, false);
 }
 
 /**
- * @tc.name: UpdateFirewallPolicyList001
- * @tc.desc: Test UpdateFirewallPolicyList Func.
+ * @tc.name: AddBackgroundPolicy001
+ * @tc.desc: Test AddBackgroundPolicy WriteFirewallRules.
  * @tc.type: FUNC
  */
-HWTEST_F(UtNetPolicyFirewall, UpdateFirewallPolicyList001, TestSize.Level1)
+HWTEST_F(UtNetPolicyFile, AddBackgroundPolicy001, TestSize.Level1)
 {
-    uint32_t chainType = 17;
-    std::vector<uint32_t> uids = {1, 2, 3};
-    bool isAllowed = true;
-    netPolicyFirewall_->powerSaveAllowedList_.clear();
-    netPolicyFirewall_->UpdateFirewallPolicyList(chainType, uids, isAllowed);
-    EXPECT_EQ(netPolicyFirewall_->powerSaveAllowedList_.size(), 3);
-}
-
-/**
- * @tc.name: UpdatePowerSavePolicy001
- * @tc.desc: Test UpdatePowerSavePolicy Func.
- * @tc.type: FUNC
- */
-HWTEST_F(UtNetPolicyFirewall, UpdatePowerSavePolicy001, TestSize.Level1)
-{
-    bool enable = false;
-    netPolicyFirewall_->powerSaveMode_ = true;
-    auto result = netPolicyFirewall_->UpdatePowerSavePolicy(enable);
-    EXPECT_NE(result, NETMANAGER_ERR_STATUS_EXIST);
-    EXPECT_NE(result, NETMANAGER_ERR_LOCAL_PTR_NULL);
-}
-
-/**
- * @tc.name: ResetPolicies001
- * @tc.desc: Test ResetPolicies ResetPolicies Func.
- * @tc.type: FUNC
- */
-HWTEST_F(UtNetPolicyFirewall, ResetPolicies001, TestSize.Level1)
-{
-    EXPECT_NE(netPolicyFirewall_->powerSaveFirewallRule_, nullptr);
-    netPolicyFirewall_->powerSaveFirewallRule_ = nullptr;
-    netPolicyFirewall_->ResetPolicies();
-}
-
-/**
- * @tc.name: HandleEvent001
- * @tc.desc: Test NetPolicyFirewall HandleEvent Func.
- * @tc.type: FUNC
- */
-HWTEST_F(UtNetPolicyFirewall, HandleEvent001, TestSize.Level1)
-{
-    const uint32_t uid = 101;
-    netPolicyFirewall_->SetDeviceIdleTrustlist({uid}, true);
-    std::vector<uint32_t> allowedList;
-    netPolicyFirewall_->GetDeviceIdleTrustlist(allowedList);
-    ASSERT_TRUE(std::find(allowedList.begin(), allowedList.end(), uid) != allowedList.end());
-    auto policyEvent = std::make_shared<PolicyEvent>();
-    netPolicyFirewall_->HandleEvent(NetPolicyEventHandler::MSG_POWER_SAVE_MODE_CHANGED, policyEvent);
-    netPolicyFirewall_->HandleEvent(NetPolicyEventHandler::MSG_DEVICE_IDLE_MODE_CHANGED, policyEvent);
-    netPolicyFirewall_->HandleEvent(NetPolicyEventHandler::MSG_DEVICE_IDLE_LIST_UPDATED, policyEvent);
-    ASSERT_FALSE(std::find(allowedList.begin(), allowedList.end(), uid) == allowedList.end());
+    netPolicyFile_->netPolicy_.backgroundPolicyStatus = "";
+    netPolicyFile_->AddBackgroundPolicy(nullptr);
+    EXPECT_NE(netPolicyFile_->netPolicy_.backgroundPolicyStatus, "");
 }
 } // namespace NetManagerStandard
 } // namespace OHOS
