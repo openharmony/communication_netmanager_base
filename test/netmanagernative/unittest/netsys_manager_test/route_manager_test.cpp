7ec79eca47512934b6667b37ad0533ffdbe15ba0
zhengsiwen960323 <zhengsiwen2@huawei.com>
2025-06-12 12:04:36 +0000

    update test/netmanagernative/unittest/netsys_manager_test/route_manager_test.cpp.
    Signed-off-by: zhengsiwen960323 <zhengsiwen2@huawei.com>

--- test/netmanagernative/unittest/netsys_manager_test/route_manager_test.cpp
+++ test/netmanagernative/unittest/netsys_manager_test/route_manager_test.cpp
@@ -739,7 +739,7 @@ HWTEST_F(RouteManagerTest, UpdateSharingNetworkTest001, TestSize.Level1)
     interfaceToTable[inputInterface] = 1;
     RouteManager::interfaceToTable_ = interfaceToTable;
     ret = RouteManager::UpdateSharingNetwork(netId, inputInterface, outputInterface);
-    EXPECT_NE(ret, NETMANAGER_ERR_INTERNAL);
+    EXPECT_NE(ret, 0);
 }
 
 HWTEST_F(RouteManagerTest, UpdateRuleInfoTest001, TestSize.Level1)
