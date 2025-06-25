88bacafa4a7d60c3f0db95bf7ce7ff4593e9209e
陈暄翊 <chenxuanyi8@huawei.com>
2025-06-25 10:12:28 +0000

    update frameworks/js/napi/netpolicy/src/bpf_stats.cpp.
    Signed-off-by: 陈暄翊 <chenxuanyi8@huawei.com>

--- frameworks/js/napi/netpolicy/src/bpf_stats.cpp
+++ frameworks/js/napi/netpolicy/src/bpf_stats.cpp
@@ -62,6 +62,7 @@ int32_t NetsysBpfStats::GetNumberFromStatsValue(uint64_t &stats, StatsType stats
 int32_t NetsysBpfStats::GetTotalStats(uint64_t &stats, StatsType statsType)
 {
     stats = 0;
+    std::lock_guard<std::mutex> lock(ifaceStatsMapMutext_);
     BpfMapper<iface_stats_key, iface_stats_value> ifaceStatsMap(IFACE_STATS_MAP_PATH, BPF_F_RDONLY);
     if (!ifaceStatsMap.IsValid()) {
         NETNATIVE_LOGE("ifaceStatsMap IsValid");
@@ -230,9 +231,9 @@ int32_t NetsysBpfStats::DeleteStatsInfo(const std::string &path, uint32_t uid)
 int32_t NetsysBpfStats::GetIfaceStats(uint64_t &stats, const StatsType statsType, const std::string &interfaceName)
 {
     stats = 0;
-    auto ifaceStatsMap =
-        std::make_shared<BpfMapper<iface_stats_key, iface_stats_value>>(IFACE_STATS_MAP_PATH, BPF_F_RDONLY);
-    if (!ifaceStatsMap->IsValid()) {
+    std::lock_guard<std::mutex> lock(ifaceStatsMapMutext_);
+    BpfMapper<iface_stats_key, iface_stats_value> ifaceStatsMap(IFACE_STATS_MAP_PATH, BPF_F_RDONLY);
+    if (!ifaceStatsMap.IsValid()) {
         return STATS_ERR_INVALID_IFACE_NAME_MAP;
     }
 
@@ -242,7 +243,7 @@ int32_t NetsysBpfStats::GetIfaceStats(uint64_t &stats, const StatsType statsType
     }
 
     iface_stats_value ifaceStats = {0};
-    if (ifaceStatsMap->Read(ifIndex, ifaceStats) < 0) {
+    if (ifaceStatsMap.Read(ifIndex, ifaceStats) < 0) {
         return STATS_ERR_READ_BPF_FAIL;
     }
     return GetNumberFromStatsValue(stats, statsType, ifaceStats);
