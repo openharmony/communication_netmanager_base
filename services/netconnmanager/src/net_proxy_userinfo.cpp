19586b26dca7a7d315883881b584bf647adfb60f
jxw <jiexiwen@huawei.com>
2025-07-29 11:01:12 +0000

    update services/netconnmanager/src/net_proxy_userinfo.cpp.
    Signed-off-by: jxw <jiexiwen@huawei.com>

--- services/netconnmanager/src/net_proxy_userinfo.cpp
+++ services/netconnmanager/src/net_proxy_userinfo.cpp
@@ -121,9 +121,7 @@ void NetProxyUserinfo::GetHttpProxyHostPass(HttpProxy &httpProxy)
     auto resultSet = rdbStore_->Query(dirAbsPred, columns);
     if (resultSet == nullptr) {
         NETMGR_LOG_E("net_conn database rdb store query failed - null resultSet");
-        if (resultSet != nullptr) {
-            resultSet->Close();
-        }
+        resultSet->Close();
         return;
     }
     if (resultSet->GoToFirstRow() != NativeRdb::E_OK) {
