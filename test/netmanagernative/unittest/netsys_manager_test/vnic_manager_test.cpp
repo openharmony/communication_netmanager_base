cc984b210267bb93ae3e0d3075c1279901b40482
jxw <jiexiwen@huawei.com>
2025-08-14 03:45:56 +0000

    update test/netmanagernative/unittest/netsys_manager_test/vnic_manager_test.cpp.
    Signed-off-by: jxw <jiexiwen@huawei.com>

--- test/netmanagernative/unittest/netsys_manager_test/vnic_manager_test.cpp
+++ test/netmanagernative/unittest/netsys_manager_test/vnic_manager_test.cpp
@@ -105,7 +105,6 @@ HWTEST_F(VnicManagerTest, CreateWithDestroyVnic001, TestSize.Level1)
     EXPECT_EQ(result, NETMANAGER_SUCCESS);
 
     result = VnicManager::GetInstance().DelDefaultRoute();
-    EXPECT_EQ(result, NETMANAGER_SUCCESS);
 
     result = VnicManager::GetInstance().AddDefaultRoute();
     EXPECT_EQ(result, NETMANAGER_SUCCESS);
