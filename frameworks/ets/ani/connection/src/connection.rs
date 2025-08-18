1b83f5f187301c30228811c65532a35efc7a5644
s00659936 <282229496@qq.com>
2025-08-18 18:24:07 +0800

    repair netHandle.getAddressByName/getAddressesByName
    Signed-off-by: s00659936 <282229496@qq.com>
    Change-Id: I4afa0a9a8602d6c42ba3fc0832d1a2239ad55c70

--- frameworks/ets/ani/connection/src/connection.rs
+++ frameworks/ets/ani/connection/src/connection.rs
@@ -113,17 +113,23 @@ pub(crate) fn get_connection_properties(
 
 #[ani_rs::native]
 pub(crate) fn get_addresses_by_name(host: String) -> Result<Vec<NetAddress>, BusinessError> {
-    let net_handle = NetConnClient::get_default_net_handle().map_err(convert_to_business_error)?;
-    NetConnClient::get_addresses_by_name(&host, net_handle.net_id)
-        .map_err(convert_to_business_error)
+    NetConnClient::get_addresses_by_name(&host, 0).map_err(convert_to_business_error)
 }
 
 #[ani_rs::native]
-pub(crate) fn get_address_by_name(
+pub(crate) fn get_address_by_name_with_handle(
     this: NetHandle,
     host: String,
 ) -> Result<NetAddress, BusinessError> {
     NetConnClient::get_address_by_name(&host, this.net_id).map_err(convert_to_business_error)
+}
+
+#[ani_rs::native]
+pub(crate) fn get_addresses_by_name_with_handle(
+    this: NetHandle,
+    host: String,
+) -> Result<Vec<NetAddress>, BusinessError> {
+    NetConnClient::get_addresses_by_name(&host, this.net_id).map_err(convert_to_business_error)
 }
 
 #[ani_rs::native]
