a527e96ae0394a4fcb7fe5acc982e9a42841ce16
leo9001 <wanglong201@h-partners.com>
2025-08-22 11:57:15 +0800

    [issue]statistics ani
    [issue]statistics ani
    
    Issue: https://gitee.com/openharmony/communication_netmanager_base/issues/ICPA0X
    
    Signed-off-by: leo9001 <wanglong201@h-partners.com>

--- frameworks/ets/ani/statistics/src/cxx/statistics_ani.cpp
+++ frameworks/ets/ani/statistics/src/cxx/statistics_ani.cpp
@@ -14,8 +14,9 @@
  */
 
 #include "statistics_ani.h"
-#include "net_manager_constants.h"
 #include "errorcode_convertor.h"
+#include "net_manager_constants.h"
+#include "wrapper.rs.h"
 
 namespace OHOS {
 namespace NetManagerAni {
@@ -31,5 +32,122 @@ NetManagerStandard::NetStatsClient &GetNetStatsClient(int32_t &nouse)
     return NetManagerStandard::NetStatsClient::GetInstance();
 }
 
+StatisEventCallback::StatisEventCallback(rust::Box<StatisticsCallback> &&callback) : callback_(std::move(callback)) {}
+
+int32_t StatisEventCallback::NetIfaceStatsChanged(const std::string &iface)
+{
+    NetStatsChangeInfo info{
+        .iface = rust::string(iface),
+    };
+    return callback_->net_iface_stats_changed(info);
+}
+
+int32_t StatisEventCallback::NetUidStatsChanged(const std::string &iface, uint32_t uid)
+{
+    NetStatsChangeInfo info{
+        .iface = rust::string(iface),
+        .uid = uid,
+    };
+    return callback_->net_uid_stats_changed(info);
+}
+
+std::unique_ptr<StatisCallbackUnregister> RegisterStatisCallback(rust::Box<StatisticsCallback> callback, int32_t &ret)
+{
+    auto eventCallback = sptr<StatisEventCallback>::MakeSptr(std::move(callback));
+    ret = NetManagerStandard::NetStatsClient::GetInstance().RegisterNetStatsCallback(eventCallback);
+    return std::make_unique<StatisCallbackUnregister>(eventCallback);
+}
+
+StatisCallbackUnregister::StatisCallbackUnregister(sptr<StatisEventCallback> eventCallback)
+    : eventCallback_(eventCallback)
+{
+}
+
+int32_t StatisCallbackUnregister::Unregister() const
+{
+    auto ret = NetManagerStandard::NetStatsClient::GetInstance().UnregisterNetStatsCallback(eventCallback_);
+    return ret;
+}
+
+NetStatsInfoInner GetTrafficStatsByIface(IfaceInfo &info, int32_t &ret)
+{
+    NetStatsInfo netStatsInfo;
+    ret = NetManagerStandard::NetStatsClient::GetInstance().GetIfaceStatsDetail(
+        std::string(info.iface), info.start_time, info.end_time, netStatsInfo);
+    if (ret != 0) {
+        return NetStatsInfoInner{};
+    }
+    return NetStatsInfoInner{.rx_bytes = netStatsInfo.rxBytes_,
+                             .tx_bytes = netStatsInfo.txBytes_,
+                             .rx_packets = netStatsInfo.rxPackets_,
+                             .tx_packets = netStatsInfo.rxPackets_};
+}
+
+NetStatsInfoInner GetTrafficStatsByUid(UidInfo &info, int32_t &ret)
+{
+    NetStatsInfo netStatsInfo;
+    ret = NetManagerStandard::NetStatsClient::GetInstance().GetUidStatsDetail(std::string(info.iface_info.iface),
+                                                                              info.uid, info.iface_info.start_time,
+                                                                              info.iface_info.end_time, netStatsInfo);
+    if (ret != 0) {
+        return NetStatsInfoInner{};
+    }
+    return NetStatsInfoInner{.rx_bytes = netStatsInfo.rxBytes_,
+                             .tx_bytes = netStatsInfo.txBytes_,
+                             .rx_packets = netStatsInfo.rxPackets_,
+                             .tx_packets = netStatsInfo.rxPackets_};
+}
+
+int32_t GetTrafficStatsByNetworkVec(AniNetworkInfo &networkInfo, rust::Vec<AniUidNetStatsInfoPair> &netStatsInfos)
+{
+    std::unordered_map<uint32_t, NetManagerStandard::NetStatsInfo> map_infos;
+    sptr<NetManagerStandard::NetStatsNetwork> networkPtr = new NetManagerStandard::NetStatsNetwork();
+    networkPtr->type_ = static_cast<uint32_t>(networkInfo.type_);
+    networkPtr->startTime_ = static_cast<uint64_t>(networkInfo.start_time);
+    networkPtr->endTime_ = static_cast<uint64_t>(networkInfo.end_time);
+    networkPtr->simId_ = static_cast<uint32_t>(networkInfo.sim_id);
+    int32_t ret = DelayedSingleton<NetManagerStandard::NetStatsClient>::GetInstance()->GetTrafficStatsByNetwork(
+        map_infos, networkPtr);
+    if (ret != 0) {
+        return ret;
+    }
+    for (auto &item : map_infos) {
+        netStatsInfos.push_back(OHOS::NetManagerAni::AniUidNetStatsInfoPair{
+            .uid = static_cast<int32_t>(item.first),
+            .net_stats_info = NetStatsInfoInner{.rx_bytes = item.second.rxBytes_,
+                                                .tx_bytes = item.second.txBytes_,
+                                                .rx_packets = item.second.rxPackets_,
+                                                .tx_packets = item.second.rxPackets_}});
+    }
+
+    return 0;
+}
+
+int32_t GetTrafficStatsByUidNetworkVec(rust::Vec<AniNetStatsInfoSequenceItem> &netStatsInfosSequence, uint32_t uid,
+                                       AniNetworkInfo &networkInfo)
+{
+    std::vector<NetManagerStandard::NetStatsInfoSequence> netStatsInfosSequenceVec;
+    sptr<NetManagerStandard::NetStatsNetwork> networkPtr = new NetManagerStandard::NetStatsNetwork();
+    networkPtr->type_ = static_cast<uint32_t>(networkInfo.type_);
+    networkPtr->startTime_ = static_cast<uint64_t>(networkInfo.start_time);
+    networkPtr->endTime_ = static_cast<uint64_t>(networkInfo.end_time);
+    networkPtr->simId_ = static_cast<uint32_t>(networkInfo.sim_id);
+    int32_t ret = DelayedSingleton<NetManagerStandard::NetStatsClient>::GetInstance()->GetTrafficStatsByUidNetwork(
+        netStatsInfosSequenceVec, uid, networkPtr);
+    if (ret != 0) {
+        return ret;
+    }
+
+    for (auto &item : netStatsInfosSequenceVec) {
+        netStatsInfosSequence.push_back(
+            AniNetStatsInfoSequenceItem{.start_time = static_cast<int64_t>(item.startTime_),
+                                        .end_time = static_cast<int64_t>(item.endTime_),
+                                        .info = NetStatsInfoInner{.rx_bytes = item.info_.rxBytes_,
+                                                                  .tx_bytes = item.info_.txBytes_,
+                                                                  .rx_packets = item.info_.rxPackets_,
+                                                                  .tx_packets = item.info_.txPackets_}});
+    }
+    return 0;
+}
 } // namespace NetManagerAni
 } // namespace OHOS
--- frameworks/ets/ani/statistics/src/wrapper.rs
+++ frameworks/ets/ani/statistics/src/wrapper.rs
@@ -11,7 +11,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use crate::bridge;
+use crate::register::StatisticsCallback;
 use cxx::let_cxx_string;
+use cxx::UniquePtr;
+use ffi::NetStatsChangeInfo;
 
 pub struct NetStatsClient;
 
@@ -117,16 +121,282 @@ impl NetStatsClient {
         }
         Ok(bytes)
     }
+
+    pub fn get_traffic_stats_by_iface(
+        iface_info: bridge::IfaceInfo,
+    ) -> Result<bridge::NetStatsInfo, i32> {
+        let mut ret = 0;
+        let info = ffi::GetTrafficStatsByIface(&mut iface_info.into(), &mut ret);
+        if ret != 0 {
+            return Err(ret);
+        }
+        Ok(info.into())
+    }
+
+    pub fn get_traffic_stats_by_uid(
+        uid_info: bridge::UidInfo,
+    ) -> Result<bridge::NetStatsInfo, i32> {
+        let mut ret = 0;
+        let info = ffi::GetTrafficStatsByUid(&mut uid_info.into(), &mut ret);
+        if ret != 0 {
+            return Err(ret);
+        }
+        Ok(info.into())
+    }
+
+    pub fn get_traffic_stats_by_network(
+        mut network_info: bridge::AniNetworkInfo,
+    ) -> Result<Vec<bridge::AniUidNetStatsInfoPair>, i32> {
+        let mut net_stats_infos_vec: std::vec::Vec<ffi::AniUidNetStatsInfoPair> = Vec::new();
+        let ret =
+            ffi::GetTrafficStatsByNetworkVec(&mut network_info.into(), &mut net_stats_infos_vec);
+        if ret != 0 {
+            return Err(ret);
+        }
+        Ok(net_stats_infos_vec.into_iter().map(Into::into).collect())
+    }
+
+    pub fn get_traffic_stats_by_uid_network(
+        uid: i32,
+        mut network_info: bridge::AniNetworkInfo,
+    ) -> Result<Vec<bridge::AniNetStatsInfoSequenceItem>, i32> {
+        let mut net_stats_info_sequence: std::vec::Vec<ffi::AniNetStatsInfoSequenceItem> =
+            Vec::new();
+
+        let ret = ffi::GetTrafficStatsByUidNetworkVec(
+            &mut net_stats_info_sequence,
+            uid.try_into().unwrap(),
+            &mut network_info.into(),
+        );
+        if ret != 0 {
+            return Err(ret);
+        }
+
+        Ok(net_stats_info_sequence
+            .into_iter()
+            .map(Into::into)
+            .collect())
+    }
+
+    pub fn register_statis_callback(
+        callback: StatisticsCallback,
+    ) -> Result<StatisCallbackUnregister, i32> {
+        let mut ret = 0;
+        let unregister = ffi::RegisterStatisCallback(Box::new(callback), &mut ret);
+        if ret != 0 {
+            return Err(ret);
+        }
+        Ok(StatisCallbackUnregister::new(unregister))
+    }
+}
+
+pub struct StatisCallbackUnregister {
+    inner: UniquePtr<ffi::StatisCallbackUnregister>,
+}
+
+unsafe impl Send for StatisCallbackUnregister {}
+unsafe impl Sync for StatisCallbackUnregister {}
+
+impl StatisCallbackUnregister {
+    fn new(inner: UniquePtr<ffi::StatisCallbackUnregister>) -> Self {
+        Self { inner }
+    }
+
+    pub fn unregister(&mut self) -> Result<(), i32> {
+        let ret = self.inner.pin_mut().Unregister();
+        if ret != 0 {
+            return Err(ret);
+        }
+        Ok(())
+    }
+}
+
+impl From<ffi::NetStatsChangeInfo> for bridge::NetStatsChangeInfo {
+    fn from(info: ffi::NetStatsChangeInfo) -> Self {
+        bridge::NetStatsChangeInfo {
+            iface: info.iface,
+            uid: Some(info.uid),
+        }
+    }
+}
+
+impl From<ffi::NetStatsInfoInner> for bridge::NetStatsInfo {
+    fn from(info: ffi::NetStatsInfoInner) -> Self {
+        bridge::NetStatsInfo {
+            rx_bytes: info.rx_bytes,
+            tx_bytes: info.tx_bytes,
+            rx_packets: info.rx_packets,
+            tx_packets: info.tx_packets,
+        }
+    }
+}
+
+impl From<ffi::NetBearType> for bridge::NetBearType {
+    fn from(value: ffi::NetBearType) -> Self {
+        match value {
+            ffi::NetBearType::BEARER_CELLULAR => bridge::NetBearType::BearerCellular,
+            ffi::NetBearType::BEARER_WIFI => bridge::NetBearType::BearerWifi,
+            ffi::NetBearType::BEARER_BLUETOOTH => bridge::NetBearType::BearerBluetooth,
+            ffi::NetBearType::BEARER_ETHERNET => bridge::NetBearType::BearerEthernet,
+            ffi::NetBearType::BEARER_VPN => bridge::NetBearType::BearerVpn,
+            ffi::NetBearType::BEARER_WIFI_AWARE => bridge::NetBearType::BearerWifiAware,
+            ffi::NetBearType::BEARER_DEFAULT => bridge::NetBearType::BearerDefault,
+            _ => unimplemented!(),
+        }
+    }
+}
+
+impl From<bridge::NetBearType> for ffi::NetBearType {
+    fn from(value: bridge::NetBearType) -> Self {
+        match value {
+            bridge::NetBearType::BearerCellular => ffi::NetBearType::BEARER_CELLULAR,
+            bridge::NetBearType::BearerWifi => ffi::NetBearType::BEARER_WIFI,
+            bridge::NetBearType::BearerBluetooth => ffi::NetBearType::BEARER_BLUETOOTH,
+            bridge::NetBearType::BearerEthernet => ffi::NetBearType::BEARER_ETHERNET,
+            bridge::NetBearType::BearerVpn => ffi::NetBearType::BEARER_VPN,
+            bridge::NetBearType::BearerWifiAware => ffi::NetBearType::BEARER_WIFI_AWARE,
+            bridge::NetBearType::BearerDefault => ffi::NetBearType::BEARER_DEFAULT,
+            _ => unimplemented!(),
+        }
+    }
+}
+
+impl From<ffi::AniNetworkInfo> for bridge::AniNetworkInfo {
+    fn from(info: ffi::AniNetworkInfo) -> Self {
+        bridge::AniNetworkInfo {
+            type_: info.type_.into(),
+            start_time: info.start_time,
+            end_time: info.end_time,
+            sim_id: Some(info.sim_id),
+        }
+    }
+}
+
+impl From<bridge::AniNetworkInfo> for ffi::AniNetworkInfo {
+    fn from(info: bridge::AniNetworkInfo) -> Self {
+        ffi::AniNetworkInfo {
+            type_: info.type_.into(),
+            start_time: info.start_time,
+            end_time: info.end_time,
+            sim_id: info.sim_id.unwrap_or_default(),
+        }
+    }
+}
+
+impl From<ffi::AniUidNetStatsInfoPair> for bridge::AniUidNetStatsInfoPair {
+    fn from(value: ffi::AniUidNetStatsInfoPair) -> Self {
+        bridge::AniUidNetStatsInfoPair {
+            uid: value.uid,
+            net_stats_info: value.net_stats_info.into(),
+        }
+    }
+}
+
+impl From<ffi::AniNetStatsInfoSequenceItem> for bridge::AniNetStatsInfoSequenceItem {
+    fn from(value: ffi::AniNetStatsInfoSequenceItem) -> Self {
+        bridge::AniNetStatsInfoSequenceItem {
+            start_time: value.start_time,
+            end_time: value.end_time,
+            info: value.info.into(),
+        }
+    }
+}
+
+impl From<bridge::IfaceInfo> for ffi::IfaceInfo {
+    fn from(info: bridge::IfaceInfo) -> Self {
+        ffi::IfaceInfo {
+            iface: info.iface,
+            start_time: info.start_time,
+            end_time: info.end_time,
+        }
+    }
+}
+
+impl From<bridge::UidInfo> for ffi::UidInfo {
+    fn from(info: bridge::UidInfo) -> Self {
+        ffi::UidInfo {
+            uid: info.uid,
+            iface_info: info.iface_info.into(),
+        }
+    }
 }
 
 #[cxx::bridge(namespace = "OHOS::NetManagerAni")]
 pub mod ffi {
+    #[namespace = "OHOS::NetManagerStandard"]
+    #[repr(i32)]
+    enum NetBearType {
+        BEARER_CELLULAR = 0,
+        BEARER_WIFI = 1,
+        BEARER_BLUETOOTH = 2,
+        BEARER_ETHERNET = 3,
+        BEARER_VPN = 4,
+        BEARER_WIFI_AWARE = 5,
+        BEARER_DEFAULT,
+    }
+
+    pub struct AniNetworkInfo {
+        pub type_: NetBearType,
+        pub start_time: i32,
+        pub end_time: i32,
+        pub sim_id: i32,
+    }
+
+    pub struct NetStatsChangeInfo {
+        pub iface: String,
+        pub uid: i32,
+    }
+    pub struct NetStatsInfoInner {
+        pub rx_bytes: i64,
+        pub tx_bytes: i64,
+        pub rx_packets: i64,
+        pub tx_packets: i64,
+    }
+
+    pub struct AniNetStatsInfoSequenceItem {
+        pub start_time: i32,
+        pub end_time: i32,
+        pub info: NetStatsInfoInner,
+    }
+
+    pub struct AniUidNetStatsInfoPair {
+        pub uid: i32,
+        pub net_stats_info: NetStatsInfoInner,
+    }
+
+    pub struct IfaceInfo {
+        pub iface: String,
+        pub start_time: i32,
+        pub end_time: i32,
+    }
+
+    pub struct UidInfo {
+        pub iface_info: IfaceInfo,
+        pub uid: i32,
+    }
+
+    extern "Rust" {
+        type StatisticsCallback;
+        pub fn net_iface_stats_changed(self: &StatisticsCallback, info: NetStatsChangeInfo) -> i32;
+        pub fn net_uid_stats_changed(self: &StatisticsCallback, info: NetStatsChangeInfo) -> i32;
+    }
+
     unsafe extern "C++" {
         include!("net_stats_client.h");
         include!("statistics_ani.h");
 
         #[namespace = "OHOS::NetManagerStandard"]
         type NetStatsClient;
+        #[namespace = "OHOS::NetManagerStandard"]
+        type NetStatsInfo;
+        type StatisCallbackUnregister;
+
+        fn RegisterStatisCallback(
+            callback: Box<StatisticsCallback>,
+            ret: &mut i32,
+        ) -> UniquePtr<StatisCallbackUnregister>;
+
+        fn Unregister(self: &StatisCallbackUnregister) -> i32;
 
         fn GetNetStatsClient(_: &mut i32) -> Pin<&'static mut NetStatsClient>;
 
@@ -159,6 +429,21 @@ pub mod ffi {
             self: Pin<&'static mut NetStatsClient>,
             bytes: &mut u64,
             sockfd: i32,
+        ) -> i32;
+
+        fn GetTrafficStatsByIface(info: &mut IfaceInfo, ret: &mut i32) -> NetStatsInfoInner;
+
+        fn GetTrafficStatsByUid(uidInfo: &mut UidInfo, ret: &mut i32) -> NetStatsInfoInner;
+
+        fn GetTrafficStatsByNetworkVec(
+            networkInfo: &mut AniNetworkInfo,
+            infos: &mut Vec<AniUidNetStatsInfoPair>,
+        ) -> i32;
+
+        fn GetTrafficStatsByUidNetworkVec(
+            net_stats_info_sequence: &mut Vec<AniNetStatsInfoSequenceItem>,
+            uid: u32,
+            networkInfo: &mut AniNetworkInfo,
         ) -> i32;
 
         fn GetErrorCodeAndMessage(error_code: &mut i32) -> String;
