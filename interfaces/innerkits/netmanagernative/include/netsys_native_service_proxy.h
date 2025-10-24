bce76249cc81785d48d7c28bff8b2e058215be1e
wendan <wendan4@huawei.com>
2025-10-24 18:29:23 +0800

    解冲突2
    Signed-off-by: wendan <wendan4@huawei.com>

--- interfaces/innerkits/netmanagernative/include/netsys_native_service_proxy.h
+++ interfaces/innerkits/netmanagernative/include/netsys_native_service_proxy.h
@@ -186,6 +186,8 @@ public:
 #ifdef FEATURE_ENTERPRISE_ROUTE_CUSTOM
     int32_t UpdateEnterpriseRoute(const std::string &interfaceName, uint32_t uid, bool add) override;
 #endif
+    int32_t SetInternetAccessByIpForWifiShare(	
+        const std::string &ipAddr, uint8_t family, bool accessInternet, const std::string &clientNetIfName) override;
     int32_t GetIpNeighTable(std::vector<NetManagerStandard::NetIpMacInfo> &ipMacInfo) override;
 
 private:
