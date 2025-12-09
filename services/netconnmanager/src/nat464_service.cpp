72fa7ebcd502604e82e0ae2a996f484bf06f5e24
weixin_43801564 <764088276@qq.com>
2025-12-09 22:27:58 +0800

    update: 更新文件 nat464_service.cpp
    Signed-off-by: weixin_43801564 <764088276@qq.com>

--- services/netconnmanager/src/nat464_service.cpp
+++ services/netconnmanager/src/nat464_service.cpp
@@ -52,7 +52,7 @@ void Nat464Service::UpdateService(Nat464UpdateFlag updateFlag)
 {
     std::weak_ptr<Nat464Service> wp = shared_from_this();
     auto handle = serviceUpdateQueue_.submit_h([wp, updateFlag]() { 
-        if(auto sharedSelf = wp.lock()) {
+        if (auto sharedSelf = wp.lock()) {
             sharedSelf->UpdateServiceState(updateFlag);
         }
     }, ffrt::task_attr().name("UpdateNat464ServiceState"));
@@ -99,7 +99,8 @@ void Nat464Service::StartPrefixDiscovery()
         if (auto sharedSelf = wp.lock()) {
             sharedSelf->DiscoverPrefix();
         }
-    }, {}, {}, ffrt::task_attr().name(("Prefix64DiscoveryIter" + std::to_string(discoveryIter_)).c_str()));
+    }, {}, {}, ffrt::task_attr().
+                name(("Prefix64DiscoveryIter" + std::to_string(discoveryIter_)).c_str()));
 }
 
 void Nat464Service::DiscoverPrefix()
@@ -128,7 +129,8 @@ void Nat464Service::DiscoverPrefix()
             if (auto sharedSelf = wp.lock()) {
                 sharedSelf->DiscoverPrefix();
             }
-        }, {}, {}, ffrt::task_attr().name(("Prefix64DiscoveryIter" + std::to_string(discoveryIter_)).c_str()));
+        }, {}, {}, ffrt::task_attr().
+                    name(("Prefix64DiscoveryIter" + std::to_string(discoveryIter_)).c_str()));
     }
 }
 
