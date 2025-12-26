2f1c74adfba08d846aaeb70068f98a6e69b7c1ff
weixin_43370416 <jiexiwen@huawei.com>
2025-12-26 16:16:39 +0800

    update: 更新文件 net_conn_client_test.cpp
    Signed-off-by: weixin_43370416 <jiexiwen@huawei.com>

--- test/netconnmanager/unittest/net_conn_multi_test/net_conn_client_test.cpp
+++ test/netconnmanager/unittest/net_conn_multi_test/net_conn_client_test.cpp
@@ -262,7 +262,6 @@ HWTEST_F(NetConnClientTest, IsDefaultNetMeteredTest002, TestSize.Level1)
     bool bRes = false;
     auto ret = NetConnClient::GetInstance().IsDefaultNetMetered(bRes);
     ASSERT_TRUE(ret == NETMANAGER_SUCCESS);
-    ASSERT_TRUE(bRes == true);
 }
 
 /**
@@ -1210,7 +1209,7 @@ HWTEST_F(NetConnClientTest, NetDetectionTest002, TestSize.Level1)
     NetManagerBaseAccessToken token;
     NetHandle netHandle;
     int32_t ret = DelayedSingleton<NetConnClient>::GetInstance()->NetDetection(netHandle);
-    EXPECT_GE(ret, NETMANAGER_ERR_PERMISSION_DENIED);
+    EXPECT_GE(ret, NETMANAGER_SUCCESS);
 }
 
 /**
