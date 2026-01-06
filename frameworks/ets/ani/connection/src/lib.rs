4e2380c29f2000ab3c599a210b7576256ebe7d8d
yyhco <yuanyuhang5@h-partners.com>
2026-01-06 20:34:36 +0800

    fix xts
    Signed-off-by: yyhco <yuanyuhang5@h-partners.com>

--- frameworks/ets/ani/connection/src/lib.rs
+++ frameworks/ets/ani/connection/src/lib.rs
@@ -57,11 +57,11 @@ ani_constructor!(
     class "@ohos.net.connection.connection.NetConnectionInner"
     [
         "onNetAvailable": connection::on_net_available,
-        "onNetBlockStatusChange": connection::on_net_block_status_change,
+        "onNetBlockStatusChangeInner": connection::on_net_block_status_change,
         "onNetCapabilitiesChange": connection::on_net_capabilities_change,
         "onNetConnectionPropertiesChange": connection::on_net_connection_properties_change,
-        "onNetLost": connection::on_net_lost,
-        "onNetUnavailable": connection::on_net_unavailable,
+        "onNetLostInner": connection::on_net_lost,
+        "onNetUnavailableInner": connection::on_net_unavailable,
         "registerSync" : connection::register_network_change,
         "unregisterSync" : connection::unregister_network_change,
     ]
