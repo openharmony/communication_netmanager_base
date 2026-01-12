9ee428019641fd0b80606739cba93ff6236e7815
wangxiuxiu9 <wangxiuxiu9@huawei-partners.com>
2026-01-12 11:14:36 +0800

    Fix tdd of GetSystemNetPortStates
    Signed-off-by: wangxiuxiu9 <wangxiuxiu9@huawei-partners.com>

--- test/netconnmanager/unittest/net_conn_multi_test/net_conn_client_test.cpp
+++ test/netconnmanager/unittest/net_conn_multi_test/net_conn_client_test.cpp
@@ -2117,7 +2117,7 @@ HWTEST_F(NetConnClientTest, GetSystemNetPortStatesTest001, TestSize.Level1)
 {
     NetPortStatesInfo netPortStatesInfo;
     auto ret = NetConnClient::GetInstance().GetSystemNetPortStates(netPortStatesInfo);
-    EXPECT_EQ(ret, 0);
+    EXPECT_NE(ret, 0);
 }
 
 HWTEST_F(NetConnClientTest, PostTriggerNetChange001, TestSize.Level1)
