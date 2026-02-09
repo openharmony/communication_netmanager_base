d7464cda484e415fb7e32fb905dc94408e8103a4
weixin_43801564 <764088276@qq.com>
2026-02-09 09:59:05 +0800

    update: 更新文件 net_conn_service_test.cpp
    Signed-off-by: weixin_43801564 <764088276@qq.com>

--- test/netconnmanager/unittest/net_conn_manager_test/net_conn_service_test.cpp
+++ test/netconnmanager/unittest/net_conn_manager_test/net_conn_service_test.cpp
@@ -2160,5 +2160,14 @@ HWTEST_F(NetConnServiceTest, GetSystemNetPortStatesTest001, TestSize.Level1)
     auto ret = NetConnService::GetInstance()->GetSystemNetPortStates(netPortStatesInfo);
     EXPECT_EQ(ret, NETMANAGER_SUCCESS);
 }
+
+HWTEST_F(NetConnServiceTest, FindProxyForURL001, TestSize.Level1)
+{
+    std::string url = "";
+    std::string host = "";
+    std::string proxy = "11";
+    NetConnService::GetInstance()->FindProxyForURL(url, host, proxy);
+    EXPECT_TRUE(proxy.empty());
+}
 } // namespace NetManagerStandard
 } // namespace OHOS
