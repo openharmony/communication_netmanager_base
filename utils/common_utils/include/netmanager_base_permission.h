f33c9df67e485323fa0b29e2d2d0d05f7a58bd45
wangmengyao (I) <w00777918@china.huawei.com>
2026-04-13 17:17:24 +0800

    流量管理接口添加权限
    Signed-off-by: wangmengyao (I) <wangmengyao17@huawei.com>

--- utils/common_utils/include/netmanager_base_permission.h
+++ utils/common_utils/include/netmanager_base_permission.h
@@ -45,6 +45,7 @@ public:
     static bool CheckPermissionWithCache(const std::string &permissionName);
     static bool IsSystemCaller();
     static bool CheckNetSysInternalPermission(const std::string &permissionName);
+    static int32_t GetApiVersion();
 };
 } // namespace NetManagerStandard
 } // namespace OHOS
