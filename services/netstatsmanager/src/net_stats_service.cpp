67cc39be9f80b5a89ac2dcd05ae90531d1431ede
wmyao_mm <wangmengyao17@huawei.com>
2026-04-13 11:49:15 +0800

    update: 更新文件 net_stats_service.cpp
    Signed-off-by: wmyao_mm <wangmengyao17@huawei.com>

--- services/netstatsmanager/src/net_stats_service.cpp
+++ services/netstatsmanager/src/net_stats_service.cpp
@@ -1675,6 +1675,7 @@ void NetStatsService::StopTrafficOvserver()
         return;
     }
 }
+// LCOV_EXCL_STOP
 
 void NetStatsService::UpdateCurActiviteSimChanged(int32_t simId, uint64_t ifIndex)
 {
@@ -1691,6 +1692,7 @@ void NetStatsService::UpdateCurActiviteSimChanged(int32_t simId, uint64_t ifInde
     }
 }
 
+// LCOV_EXCL_START
 bool NetStatsService::IsSimIdExist(int32_t simId)
 {
     std::shared_lock<ffrt::shared_mutex> lock(simIdToIfIndexMapMutex_);
