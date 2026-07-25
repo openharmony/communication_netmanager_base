0134c594d2b008f9664caa299ea4515eafd88811
lsyhh <331993722@qq.com>
2026-07-25 16:45:51 +0800

    update: 更新文件 netmanager_base_common_utils.cpp
    Signed-off-by: lsyhh <331993722@qq.com>

--- utils/common_utils/src/netmanager_base_common_utils.cpp
+++ utils/common_utils/src/netmanager_base_common_utils.cpp
@@ -717,8 +717,6 @@ int32_t ForkExecParentProcess(const int32_t *pipeFd, int32_t count, pid_t childP
     }
     if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
         // child process abnormal exit
-        NETMGR_LOG_E("child process abnormal exit, status:%{public}d", status);
-        return NETMANAGER_ERROR;
     }
     NETMGR_LOG_I("waitpid %{public}d done", childPid);
     return NETMANAGER_SUCCESS;
