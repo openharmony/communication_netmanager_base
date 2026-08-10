e3ae040ff5307b226aef43ad6db67487113e50e5
l_pf <1321018403@qq.com>
2026-08-10 16:31:13 +0800

    告警修改
    Signed-off-by: l_pf <1321018403@qq.com>

--- services/netmanagernative/include/manager/route_manager.h
+++ services/netmanagernative/include/manager/route_manager.h
@@ -353,7 +353,7 @@ private:
         VPN_NETWORK_TABLE,
     };
 
-    static bool vpnSysCall_;
+    static std::atomic<bool> vpnSysCall_;
     static std::string defauleNetWorkName_;
 
     static bool CheckSysVpnCall();
